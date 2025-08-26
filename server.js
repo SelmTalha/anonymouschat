74d906265088525cf95b6ce82b24241178ef8260
SelmTalha <selimtalha26@gmail.com>
2025-08-26 20:31:59 +0300

    update server.js for Render port

--- server.js
+++ server.js
@@ -2,7 +2,11 @@ const express = require('express');
 const socket = require('socket.io');
 
 const app = express();
-const server = app.listen(3000, () => console.log('Server is running on port 3000'));
+
+// Render veya Heroku'nun PORT değişkenini kullan, yoksa 3000
+const PORT = process.env.PORT || 3000;
+const server = app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+
 app.use(express.static('public'));
 
 const io = socket(server);
